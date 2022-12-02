4247f4212808cbc5984f0593b714bebbb4ff969f
Rimmy Le <56008781+rimmyle@users.noreply.github.com>
2022-12-02 09:29:25 -0800

    Pricing Banner Text

--- notflix/components/advert.tsx
+++ notflix/components/advert.tsx
@@ -5,6 +5,15 @@ function Advert () {
     <>
         <div className={styles.container}>
           <div className={styles['badge-me']}>New!</div>
+          <div className={styles['ad-text']}>
+            <span>Plans now start at  
+            <span className={styles['price-text']}> $6.99</span>
+            .
+            </span>
+          </div>
+          <div className={styles['learn-button-container']}>
+            
+          </div>
         </div>
 
     </>
