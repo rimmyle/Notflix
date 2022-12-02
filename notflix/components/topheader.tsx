2187b1f1c7dc5eb6f45cb20dd6926646db507dfb
Rimmy Le <56008781+rimmyle@users.noreply.github.com>
2022-12-02 12:24:46 -0800

    Mounted Lnaguage Dropdown

--- notflix/components/topheader.tsx
+++ notflix/components/topheader.tsx
@@ -1,18 +1,33 @@
 import styles from '../styles/topheader.module.css'
 import logo from '../images/logo.png'
 import Image from 'next/Image'
+import { Dropdown } from '@nextui-org/react'
 
 function Topheader() {
     return(
         <>
             <div className={styles.wrapper}>
-                <div className={styles['sub-wrapper]']}>
-                
-                    <div className={styles.logo}>
-                        <Image src={logo} alt="notflix logo" layout="fill" />
+                <div className={styles['sub-wrapper']}>
+                    <div className={styles.separator}>
+                        <div className={styles.logo}>
+                            <Image src={logo} alt="notflix logo" layout="fill" />
+                        </div>
                     </div>
-  
+
+                    <div className={styles['select-container']}>
+                        <div className={styles['select-wrapper']}>
+                            <Dropdown>
+                                <Dropdown.Button flat>English</Dropdown.Button>
+                                <Dropdown.Menu aria-label="Static Actions">
+                                    <Dropdown.Item key="english">English</Dropdown.Item>
+                                    <Dropdown.Item key="lol">A Different Language</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Dropdown>
+                        </div>
+                    </div>
+                    
                 </div>
+                
             </div>
         </>
     )
