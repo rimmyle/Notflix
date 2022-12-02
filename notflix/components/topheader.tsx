c31984711455d504deab14016d6a0e61fc75bbc7
Rimmy Le <56008781+rimmyle@users.noreply.github.com>
2022-12-02 11:49:17 -0800

    Notflix Logo

--- notflix/components/topheader.tsx
+++ notflix/components/topheader.tsx
@@ -1,7 +1,19 @@
 import styles from '../styles/topheader.module.css'
+import logo from '../images/logo.png'
+import Image from 'next/Image'
+
 function Topheader() {
     return(
         <>
+            <div className={styles.wrapper}>
+                <div className={styles['sub-wrapper]']}>
+                
+                    <div className={styles.logo}>
+                        <Image src={logo} alt="notflix logo" layout="fill" />
+                    </div>
+  
+                </div>
+            </div>
         </>
     )
 }
--- notflix/pages/index.tsx
+++ notflix/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Advert from '../components/advert'
 import styles from '../styles/index.module.css'
+import Topheader from '../components/topheader'
 
 
 export default function Home() {
@@ -15,6 +16,7 @@ export default function Home() {
         <div className={styles["notflix-sans-font-loaded"]}>
           <div className={styles.container} dir="ltr">
             {Advert()}
+            {Topheader()}
           </div>
         
         <section>
