29f3d46045bcb710a68c863577459e2107ed8afa
Rimmy Le <56008781+rimmyle@users.noreply.github.com>
2022-12-02 19:31:51 -0800

    mounting adjustments for tv

--- notflix/components/tv.tsx
+++ notflix/components/tv.tsx
@@ -7,17 +7,19 @@ function tv() {
     return (
         <>
             <div className={styles['tv-container']}>
-                <div className={styles['text-container']}>
-                    <h1 className={styles.title}>Enjoy on your TV.</h1>
-                    <h3 className={styles.subtitle}>Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</h3>
-                </div>
+                <div className={styles.wrapper}>
+                    <div className={styles['text-container']}>
+                        <h1 className={styles.title}>Enjoy on your TV.</h1>
+                        <h3 className={styles.subtitle}>Watch on Smart TVs, Playstation, Xbox, Chromecast, Apple TV, Blu-ray players, and more.</h3>
+                    </div>
                     <div className={styles.tv}>
                         <Image className={styles['tv-image']} src={tv_pic} alt="tv" layout="fill"/>
                         <video className={styles.video} loop autoPlay muted>
                             <source src={vid} type="video/mp4"/>
                         </video>
                     </div>
-                </div>
+                </div> 
+            </div>
                 
         </>
     )
